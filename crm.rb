c6f2864a65a7ff769c762d2c5cec800a6348520e
Cody House <codyahouse@gmail.com>
2014-10-28 15:48:52 -0400

    finished part one of crm, all working well

--- crm.rb
+++ crm.rb
@@ -1,6 +1,9 @@
-reuqire_relative 'rolodex'
+require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
+
+
+	$rolodex = Rolodex.new
 
 get '/' do 
 	@crm_app_name = "My CRM"
@@ -9,14 +12,15 @@ end
 
 
 get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
   erb :contacts
 end
 
-# #post '/contacts' do
-#    erb :contacts
-# end
+get '/contacts/new' do
+	erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
